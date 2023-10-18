0c82a3c8d1f2dae469ca203560dacc005096c900
Leonardo Ringo <leonardoringo12@gmail.com>
2023-10-18 15:53:45 +0700

    feat: navbar done and fixing menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,11 @@ const Navbar: React.FC = () => {
     const router = useRouter();
 
     const isActive = (pathname: string) => {
-        return router.pathname === pathname ? 'text-blue-500 bg-red-500' : 'text-gray-500';
+        //-------- Style for navigation -------- 
+        const notActiveTab = "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-[#7e8fa9] min-h-[35px] min-w-[100px]"
+        const activeTab = "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black shadow min-h-[25px] min-w-[100px]"
+
+        return router.pathname === pathname ? activeTab : notActiveTab;
     };
 
     return (
@@ -20,7 +24,7 @@ const Navbar: React.FC = () => {
                     data-state="closed">
                     <div className="border rounded-full p-4 hover:bg-muted transition-colors cursor-pointer bg-background shadow"><svg
                         xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                         className="lucide lucide-file">
                         <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path>
                         <polyline points="14 2 14 8 20 8"></polyline>
@@ -28,19 +32,19 @@ const Navbar: React.FC = () => {
                 </button>
             </div>
             <div className="flex justify-between">
-                <div className="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground">
+                <div className="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground bg-[#f1f5f9]">
                     <Link href="/menu"
-                        className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#f1f5f9] text-[#64748b] min-h-[40px] min-w-[100px] ">Menu</Link>
+                        className={isActive('/menu')}>Menu</Link>
                     <Link href="/order"
-                        className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#f1f5f9] text-[#64748b] min-h-[40px] min-w-[100px]">Order</Link>
+                        className={isActive('/order')}>Order</Link>
                     <Link href="/dapur"
-                        className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#f1f5f9] text-[#64748b] min-h-[40px] min-w-[100px]">Dapur</Link>
+                        className={isActive('/dapur')}>Dapur</Link>
                     <Link href="/kasir"
-                        className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#f1f5f9] text-[#64748b] min-h-[40px] min-w-[100px]">Kasir</Link>
+                        className={isActive('/kasir')}>Kasir</Link>
                 </div>
                 <Link href="/menu"
                     className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 w-[100px]"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                         className="mr-2">
                         <path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
                         <path d="M3 3v5h5"></path>
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,58 +1,111 @@
 import { useEffect, useState } from "react"
+import MenuList from "@/components/MenuList";
 
-interface newMenu {
+interface menuList {
     id: string;
-    name: string
+    name: string;
 }
 
 export default function CardSandbox() {
+    const [newMenu, setNewMenu] = useState<string>("")
+    const [menuList, setMenuList] = useState<menuList[]>([])
+
     const menu = [
         { "id": "996756", "name": "Ayam Kecap Manis" },
         { "id": "362342", "name": "Nasi Goreng Spesial" }
     ]
 
-    const [newMenu, setNewmenu] = useState<newMenu>()
-
     const setLocalStorage = () => {
-        if(!localStorage.getItem('menus')) {
+        if (!localStorage.getItem('menus')) {
             localStorage.setItem('menus', JSON.stringify(menu))
         }
-        localStorage.getItem('menus')
+
+        if (!menuList) {
+            const storedData = localStorage.getItem('menus')
+            const menus: menuList[] = storedData ? JSON.parse(storedData) : []
+            setMenuList(menus)
+        }
+    }
+
+    const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNewMenu(event.target.value)
+    }
+
+    const newMenuSubmitted = (event: any) => {
+        event.preventDefault()
+        const storedData = localStorage.getItem('menus')
+        const menus: menuList[] = storedData ? JSON.parse(storedData) : []
+        const payload: menuList = {
+            id: String(Math.floor(100000 + Math.random() * 900000)),
+            name: newMenu
+        }
+        menus.push(payload)
+        localStorage.setItem('menus', JSON.stringify(menus))
     }
 
     useEffect(() => {
         setLocalStorage()
-    }, [])
+    }, [menuList])
 
     return (
-        <div>
-            <input type="text" />
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>Song</th>
-                        <th>Artist</th>
-                        <th>Year</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                    </tr>
-                    <tr>
-                        <td>Witchy Woman</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                    </tr>
-                    <tr>
-                        <td>Shining Star</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>1975</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="px-6 space-y-5 w-[650px]">
+            <div>
+                <div className="px-6 py-2 bg-[#f1f5f9] rounded-md min-h-[300px]">
+                    <section className="space-y-4">
+                        <div className="space-y-2">
+                            <div className="space-y-1">
+                                <label
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                    htmlFor="menu"
+                                >
+                                    Menu Makanan
+                                </label>
+                                <div className="flex space-x-2">
+                                    <input
+                                        className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                                        id="menu"
+                                        placeholder="Tambahkan disini..."
+                                        value={newMenu}
+                                        onChange={inputHandler}
+                                    />
+                                    <button
+                                        className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-[120px]"
+                                        onClick={newMenuSubmitted}
+                                    >
+                                        Tambah
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full overflow-auto">
+                            <table className="w-full caption-bottom text-sm">
+                                <caption className="mt-4 text-sm text-muted-foreground">Daftar menu restoran Anda</caption>
+                                <thead className="[&amp;_tr]:border-b">
+                                    <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+                                        <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 w-[100px]">
+                                            ID
+                                        </th>
+                                        <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
+                                            Menu
+                                        </th>
+                                        <th className="h-12 px-4 align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 text-right">
+                                            Hapus?
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="[&amp;_tr:last-child]:border-0">
+                                    {menuList.map(menu => (
+                                        <MenuList key={menu.id} id={menu.id} name={menu.name} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <p className="text-sm text-muted-foreground text-center">
+                Semua data hanya disimpan di Local Storage browser Anda
+            </p>
         </div>
     )
 }
